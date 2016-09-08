806b522f4716ff7a5242aaae9484a6a75a65563a
sukh dol <sukh.dol77@gmail.com>
2016-09-08 16:32:03 -0600

    Create AddLoan post method.

--- MortgageCalculator/MortgageCalculator/Controllers/Api/LoansController.cs
+++ MortgageCalculator/MortgageCalculator/Controllers/Api/LoansController.cs
@@ -1,9 +1,24 @@
-﻿using System.Web.Http;
+﻿using MortgageCalculator.Models;
+using System.Web.Http;
 
 namespace MortgageCalculator.Controllers.Api
 {
     public class LoansController : ApiController
     {
+        private ApplicationDbContext _context;
 
+        public LoansController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddLoan(Loan loan)
+        {
+            _context.Loans.Add(loan);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
