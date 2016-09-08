04db0871d868f231fa8df586030591c427830417
sukh dol <sukh.dol77@gmail.com>
2016-09-07 22:06:24 -0600

    Add Loan form view model.

---
+++ MortgageCalculator/MortgageCalculator/ViewModels/LoanFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MortgageCalculator.ViewModels
+{
+    public class LoanFormViewModel
+    {
+        public double MortgageAmount { get; set; }
+        public double AnnualInterestRate { get; set; }
+        public int AmortizationPeriod { get; set; }
+        public double MonthlyPayment { get; set; }
+    }
+}
