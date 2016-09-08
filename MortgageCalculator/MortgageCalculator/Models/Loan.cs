ba3c5b5c08030b88b2db4df15e89127fde44a3b4
sukh dol <sukh.dol77@gmail.com>
2016-09-07 21:56:21 -0600

    Add Loan model/class.

---
+++ MortgageCalculator/MortgageCalculator/Models/Loan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MortgageCalculator.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public double MortgageAmount { get; set; }
+        public double AnnualInterestRate { get; set; }
+        public int AmortizationPeriod { get; set; }
+        public double MonthlyPayment { get; set; }
+        public DateTime DateCalculated { get; set; }
+    }
+}
