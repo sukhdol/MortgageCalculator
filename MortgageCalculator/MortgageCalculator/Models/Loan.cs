c7c732229e2b00d4264a0a4515a84d51a82fcf67
sukh dol <sukh.dol77@gmail.com>
2016-09-08 16:11:10 -0600

    Add Data Annotations.

--- MortgageCalculator/MortgageCalculator/Models/Loan.cs
+++ MortgageCalculator/MortgageCalculator/Models/Loan.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MortgageCalculator.Models
 {
     public class Loan
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Mortgage Amount is required.")]
         public double MortgageAmount { get; set; }
+
+        [Required(ErrorMessage = "The Annual Interest Rate is required.")]
         public double AnnualInterestRate { get; set; }
+
+        [Required(ErrorMessage = "The Amortization Period is required.")]
         public int AmortizationPeriod { get; set; }
+
         public double MonthlyPayment { get; set; }
         public DateTime DateCalculated { get; set; }
     }
