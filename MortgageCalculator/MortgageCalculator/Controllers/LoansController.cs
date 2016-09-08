1c6758c70933caceaf600d7a9ad8b95c581612bc
sukh dol <sukh.dol77@gmail.com>
2016-09-07 22:04:30 -0600

    Add Loans controller.

---
+++ MortgageCalculator/MortgageCalculator/Controllers/LoansController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MortgageCalculator.Controllers
+{
+    public class LoansController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
