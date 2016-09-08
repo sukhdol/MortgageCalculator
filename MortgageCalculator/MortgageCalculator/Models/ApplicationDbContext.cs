66e754f92854b664ea7a634ea19257700dd422eb
sukh dol <sukh.dol77@gmail.com>
2016-09-08 16:28:53 -0600

    Add Loans and Application User tables.

--- MortgageCalculator/MortgageCalculator/Models/ApplicationDbContext.cs
+++ MortgageCalculator/MortgageCalculator/Models/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace MortgageCalculator.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Loan> Loans { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
