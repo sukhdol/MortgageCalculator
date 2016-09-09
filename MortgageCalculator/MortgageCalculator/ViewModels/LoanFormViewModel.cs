c8b8461f02d9e6c64e2d19dcaa4c4c5b63866364
sukh dol <sukh.dol77@gmail.com>
2016-09-09 15:56:39 -0600

    Hide LoanSchedule Table by default.

--- MortgageCalculator/MortgageCalculator/ViewModels/LoanFormViewModel.cs
+++ MortgageCalculator/MortgageCalculator/ViewModels/LoanFormViewModel.cs
@@ -7,13 +7,13 @@ namespace MortgageCalculator.ViewModels
         public int Id { get; set; }
 
         [Required(ErrorMessage = "The Mortgage Amount is required.")]
-        public double? MortgageAmount { get; set; }
+        public double MortgageAmount { get; set; }
 
         [Required(ErrorMessage = "The Annual Interest Rate is required.")]
-        public double? AnnualInterestRate { get; set; }
+        public double AnnualInterestRate { get; set; }
 
         [Required(ErrorMessage = "The Amortization Period is required.")]
-        public int? AmortizationPeriod { get; set; }
+        public int AmortizationPeriod { get; set; }
 
         public double MonthlyPayment { get; set; }
     }
