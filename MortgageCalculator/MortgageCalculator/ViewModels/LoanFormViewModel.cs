912e73b6906321866105693c16d4113212e05ff3
sukh dol <sukh.dol77@gmail.com>
2016-09-09 17:58:09 -0600

    Add Client-side Validation and change default values.

--- MortgageCalculator/MortgageCalculator/ViewModels/LoanFormViewModel.cs
+++ MortgageCalculator/MortgageCalculator/ViewModels/LoanFormViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MortgageCalculator.ViewModels
 {
@@ -7,12 +8,18 @@ namespace MortgageCalculator.ViewModels
         public int Id { get; set; }
 
         [Required(ErrorMessage = "The Mortgage Amount is required.")]
+        [Range(0.01, Double.MaxValue,
+                    ErrorMessage = "The Mortgage Amount should be higher than 0.")]
         public double MortgageAmount { get; set; }
 
         [Required(ErrorMessage = "The Annual Interest Rate is required.")]
+        [Range(0.01, Double.MaxValue,
+                        ErrorMessage = "The Annual Interest Rate should be higher than 0.")]
         public double AnnualInterestRate { get; set; }
 
         [Required(ErrorMessage = "The Amortization Period is required.")]
+        [Range(1, Int32.MaxValue,
+                        ErrorMessage = "The Amortization Period should be higher than 0.")]
         public int AmortizationPeriod { get; set; }
 
         public double MonthlyPayment { get; set; }
