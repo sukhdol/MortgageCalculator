cf027d084d5687e4ff86ef6764b33d65a9dcfe89
sukh dol <sukh.dol77@gmail.com>
2016-09-08 20:37:24 -0600

    Add Loan Tests.

---
+++ MortgageCalculator/MortgageCalculator.Tests/LoanTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MortgageCalculator.Models;
+
+namespace MortgageCalculator.Tests
+{
+    [TestClass]
+    public class LoanTest
+    {
+        [TestMethod]
+        public void IsGetMonthlyPaymentGivingCorrectResult1()
+        {
+            var loan = new Loan
+            {
+                MortgageAmount = 250000,
+                AnnualInterestRate = 5.29,
+                AmortizationPeriod = 25
+            };
+
+            var result = loan.GetMonthlyPayment();
+
+            Assert.AreEqual(1495.56, result);
+        }
+
+        [TestMethod]
+        public void IsGetMonthlyPaymentGivingCorrectResult2()
+        {
+            var loan = new Loan
+            {
+                MortgageAmount = 250000,
+                AnnualInterestRate = 3.09,
+                AmortizationPeriod = 25
+            };
+
+            var result = loan.GetMonthlyPayment();
+
+            Assert.AreEqual(1194.69, result);
+        }
+
+        [TestMethod]
+        public void IsGetMonthlyPaymentGivingCorrectResult3()
+        {
+            var loan = new Loan
+            {
+                MortgageAmount = 250000,
+                AnnualInterestRate = 5.29,
+                AmortizationPeriod = 20
+            };
+
+            var result = loan.GetMonthlyPayment();
+
+            Assert.AreEqual(1682.18, result);
+        }
+    }
+}
