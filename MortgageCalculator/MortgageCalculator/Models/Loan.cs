1c9b36bad3f1b9ef312899690b5df35564b0b28e
sukh dol <sukh.dol77@gmail.com>
2016-09-08 20:25:46 -0600

    Add GetMonthlyPayment method.

--- MortgageCalculator/MortgageCalculator/Models/Loan.cs
+++ MortgageCalculator/MortgageCalculator/Models/Loan.cs
@@ -18,5 +18,40 @@ namespace MortgageCalculator.Models
 
         public double MonthlyPayment { get; set; }
         public DateTime DateCalculated { get; set; }
+
+        // Calculate the Mortgage Monthly Payment
+        // Used in the Unit Test
+        public double GetMonthlyPayment()
+        {
+            var interestRate = AnnualInterestRate;
+            var mortgageAmount = MortgageAmount;
+            var amortizationPeriod = AmortizationPeriod;
+
+            // first calculation of the equation/formula
+            // using BEDMAS from left to right (Parentheses first then exponent)
+            // FORMULA: (1 + (annualInterestRate/200)) ^ (1/6)
+            // this step is used twice in the equation (numerator and denominator)
+            var firstEquationCalculation = Math.Pow((1 + (interestRate / 200.0)), (1.0 / 6.0));
+
+            // second calculation of the equation/formula
+            // FORMULA: (-12 * amortizationPeriod)
+            // used in the denominator
+            var denominatorExponent = (-12.0) * amortizationPeriod;
+
+            // final calculation for the numerator
+            // FORMULA: firstEquationCalculation - 1
+            var numerator = firstEquationCalculation - 1;
+
+            // final calculation for the denominator
+            // FORMULA: 1 - (  firstEquationCalculation ^ denominatorExponent ) )
+            var denominator = 1 - (Math.Pow(firstEquationCalculation, denominatorExponent));
+
+            // final calculation of the formula
+            // Formula: mortgageAmount * (numerator/denominator)
+            var monthlyPayment = mortgageAmount * (numerator / denominator);
+
+            return Math.Round(monthlyPayment * 100) / 100.0;
+        }
+
     }
 }
