1e77db64f7e716725a6d9730bfa7e09090d5899f
sukh dol <sukh.dol77@gmail.com>
2016-09-10 13:35:39 -0600

    Implement Email service.

---
+++ MortgageCalculator/MortgageCalculator/Models/Email.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mail;
+
+namespace MortgageCalculator.Models
+{
+    public class Email
+    {
+        private const string SenderEmail = "linusseven77@gmail.com";
+
+        public string RecipientEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public void SendEmail()
+        {
+            var mail = new MailMessage();
+
+            mail.To.Add(new MailAddress(RecipientEmail));
+            mail.From = new MailAddress(SenderEmail);
+            mail.Subject = Subject;
+            mail.IsBodyHtml = true;
+
+            var smtpServer = new SmtpClient();
+
+            smtpServer.Send(mail);
+        }
+    }
+}
