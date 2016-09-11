6da1d6790ec2b6ba4a025f5aa8af6296a961b0f9
sukh dol <sukh.dol77@gmail.com>
2016-09-10 18:40:17 -0600

    Correct toastr link address.

--- MortgageCalculator/MortgageCalculator/App_Start/BundleConfig.cs
+++ MortgageCalculator/MortgageCalculator/App_Start/BundleConfig.cs
@@ -28,7 +28,7 @@ namespace MortgageCalculator
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "/Content/toastr.css",
+                      "~/Content/toastr.css",
                       "~/Content/datatables/css/datatables.bootstrap.css"));
         }
     }
